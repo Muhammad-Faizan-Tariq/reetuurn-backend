75e8e9ed7f827ed6a59e6e8e1e6993a8c6ed458f
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-07-19 20:02:22 +0500

    Refactor user profile routes to consolidate customer creation and remove unused routes

--- src/routes/userProfile.route.js
+++ src/routes/userProfile.route.js
@@ -1,12 +1,9 @@
 import express from "express";
-import { verifyToken } from "../middlewares/auth.middleware.js";
-import { createOrUpdateProfile, registerUser } from "../controllers/userProfile.controller.js";
+import { createCustomer } from "../controllers/userProfile.controller.js";
 import { registerValidator, validate } from "../validations/auth.validation.js";
 
 const router = express.Router();
 
-
-router.post("/register", registerValidator, validate, registerUser);
-router.post("/profile", verifyToken, createOrUpdateProfile);
+router.post("/create-customer", registerValidator, validate, createCustomer);
 
 export default router;
