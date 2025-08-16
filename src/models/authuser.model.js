4c4f549fb5deae6f2067348cb505520aa9fbafe5
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-08-16 20:27:36 +0500

    Remove username field from authUser schema

--- src/models/authuser.model.js
+++ src/models/authuser.model.js
@@ -20,13 +20,6 @@ const authUserSchema = new mongoose.Schema({
     ref: "UserProfile",
     required: false,
   },
-  username: {
-    type: String,
-    required: [true, "Username is required"],
-    unique: true,
-    minlength: [3, "Username must be at least 3 characters"],
-    maxlength: [30, "Username must not exceed 30 characters"]
-  },
   email: {
     type: String,
     required: [true, "Email is required"],
