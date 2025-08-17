5b98c2940662ec7433b19646ce8905f30599d6dc
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-08-17 17:00:40 +0500

    Temporarily disable token verification and add hardcoded user for testing in return order route

--- src/routes/returnOrder.route.js
+++ src/routes/returnOrder.route.js
@@ -1,16 +1,19 @@
 import express from "express";
 import { createReturnOrder } from "../controllers/returnOrder.controller.js";
 import { validateReturnOrder } from "../validations/returnOrder.validation.js";
-import {validate} from "../validations/return.validation.js"
 import { verifyToken } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post(
   "/",
-  verifyToken,
-  validateReturnOrder, 
-  validate, 
+  //   verifyToken,
+  (req, res, next) => {
+    // Hardcode the user for testing
+    req.user = { _id: "689e1ee74d1d69d5344d0749", name: "John ali" };
+    next();
+  },
+  validateReturnOrder,
   createReturnOrder
 );
 
