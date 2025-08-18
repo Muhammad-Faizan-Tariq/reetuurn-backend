a7a59874849cd70863ba5c7d94f9ea0f85ea1ed9
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-08-18 22:24:25 +0500

    Refactor OTP schema and password history structure for improved clarity and consistency

--- src/models/authuser.model.js
+++ src/models/authuser.model.js
@@ -1,24 +1,25 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import validator from "validator";
+import crypto from "crypto";
 
 const otpSchema = new mongoose.Schema(
   {
     code: {
       type: String,
-      required: true,
       minlength: 6,
       maxlength: 6,
+      required: false,
     },
     expiry: {
       type: Date,
-      required: true,
-      default: () => new Date(Date.now() + 15 * 60 * 1000), 
+      default: null, 
+      required: false,
     },
     purpose: {
       type: String,
       enum: ["verification", "passwordReset", "phoneVerification"],
-      required: true,
+      required: false, 
     },
   },
   { _id: false }
@@ -26,14 +27,8 @@ const otpSchema = new mongoose.Schema(
 
 const passwordHistorySchema = new mongoose.Schema(
   {
-    password: {
-      type: String,
-      required: true,
-    },
-    changedAt: {
-      type: Date,
-      default: Date.now,
-    },
+    password: { type: String, required: true },
+    changedAt: { type: Date, default: Date.now },
   },
   { _id: false }
 );
@@ -68,7 +63,7 @@ const authUserSchema = new mongoose.Schema(
       type: String,
       required: [true, "Password is required"],
       minlength: [8, "Password must be at least 8 characters"],
-      select: false, 
+      select: false,
     },
     passwordHistory: [passwordHistorySchema],
     passwordChangedAt: Date,
@@ -79,20 +74,11 @@ const authUserSchema = new mongoose.Schema(
       enum: ["customer", "driver", "admin"],
       default: "customer",
     },
-    otp: otpSchema,
-    isVerified: {
-      type: Boolean,
-      default: false,
-    },
-    isActive: {
-      type: Boolean,
-      default: true,
-    },
+    otp: { type: otpSchema, default: {} },
+    isVerified: { type: Boolean, default: false },
+    isActive: { type: Boolean, default: true },
     lastLogin: Date,
-    failedLoginAttempts: {
-      type: Number,
-      default: 0,
-    },
+    failedLoginAttempts: { type: Number, default: 0 },
     lockUntil: Date,
   },
   {
@@ -107,19 +93,15 @@ authUserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
   try {
-
     this.password = await bcrypt.hash(this.password, 12);
 
-
     if (this.passwordHistory) {
-      this.passwordHistory.push({
-        password: this.password,
-      });
+      this.passwordHistory.push({ password: this.password });
     } else {
       this.passwordHistory = [{ password: this.password }];
     }
 
-    this.passwordChangedAt = Date.now() - 1000; 
+    this.passwordChangedAt = Date.now() - 1000;
     next();
   } catch (err) {
     next(err);
@@ -136,25 +118,20 @@ authUserSchema.methods.changePassword = async function (
   currentPassword,
   newPassword
 ) {
- 
   if (!(await this.comparePassword(currentPassword))) {
     throw new Error("Current password is incorrect");
   }
-
 
   if (newPassword.length < 8) {
     throw new Error("Password must be at least 8 characters");
   }
 
-
   const isUsedBefore = this.passwordHistory.some((record) =>
     bcrypt.compareSync(newPassword, record.password)
   );
-
   if (isUsedBefore) {
     throw new Error("Cannot reuse previous passwords");
   }
-
 
   this.password = newPassword;
   await this.save();
@@ -164,7 +141,7 @@ authUserSchema.methods.changePassword = async function (
 authUserSchema.methods.incrementLoginAttempts = function () {
   this.failedLoginAttempts += 1;
   if (this.failedLoginAttempts >= 5) {
-    this.lockUntil = Date.now() + 30 * 60 * 1000; 
+    this.lockUntil = Date.now() + 30 * 60 * 1000;
   }
   return this.save();
 };
@@ -182,10 +159,9 @@ authUserSchema.methods.createPasswordResetToken = function () {
     .createHash("sha256")
     .update(resetToken)
     .digest("hex");
-  this.passwordResetExpires = Date.now() + 10 * 60 * 1000; // 10 minutes
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
   return resetToken;
 };
-
 
 authUserSchema.virtual("isLocked").get(function () {
   return this.lockUntil && this.lockUntil > Date.now();
