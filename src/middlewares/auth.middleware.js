bf38974fa11f77877025a76d550b9db1786d1877
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-08-18 17:13:39 +0500

    Refactor token verification logic for improved error handling and logging

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,22 +2,30 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  if (!authHeader)
-    return res
-      .status(401)
-      .json({ success: false, message: "Unauthorized user" });
 
-  const token = authHeader.split(" ")[1]; 
-  if (!token)
+  if (!authHeader) {
+    console.log("❌ No Authorization header found");
     return res
       .status(401)
       .json({ success: false, message: "Unauthorized user" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    console.log("❌ No token found in Authorization header");
+    return res
+      .status(401)
+      .json({ success: false, message: "Unauthorized user" });
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
-    req.user = decoded; 
+    req.user = decoded;
+    console.log("✅ Token verified. Decoded user:", decoded);
     next();
   } catch (err) {
+    console.log("❌ Token verification failed:", err.message);
     return res
       .status(401)
       .json({ success: false, message: "Unauthorized user" });
