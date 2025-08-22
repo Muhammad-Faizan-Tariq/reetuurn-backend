63aa61dce1be388bb38962b4ae516362cabba7b8
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-08-23 02:53:13 +0500

    Refactor customer controller methods for improved clarity and consistency

--- src/controllers/customter.controller.js
+++ src/controllers/customter.controller.js
@@ -1,9 +1,9 @@
 import {
   getCustomerProfile,
   updatePhoneNumbers,
-  updateAddresses,
-  addNewAddress,
-  verifyPhoneNumber,
+  addPhoneNumber,
+  updateCustomerAddresses,
+  addCustomerAddress,
 } from "../services/customer.service.js";
 import { successResponse, errorResponse } from "../utils/response.util.js";
 
@@ -25,29 +25,28 @@ export const updatePhones = async (req, res) => {
   }
 };
 
-export const updateCustomerAddresses = async (req, res) => {
+export const addPhone = async (req, res) => {
   try {
-    const addresses = await updateAddresses(req.user._id, req.body.addresses);
+    const phones = await addPhoneNumber(req.user._id, req.body);
+    successResponse(res, 201, "Phone added", { phones });
+  } catch (error) {
+    errorResponse(res, 400, error.message);
+  }
+};
+
+export const updateAddresses = async (req, res) => {
+  try {
+    const addresses = await updateCustomerAddresses(req.user._id, req.body.addresses);
     successResponse(res, 200, "Addresses updated", { addresses });
   } catch (error) {
     errorResponse(res, 400, error.message);
   }
 };
 
-export const createAddress = async (req, res) => {
+export const addAddress = async (req, res) => {
   try {
-    const addresses = await addNewAddress(req.user._id, req.body);
+    const addresses = await addCustomerAddress(req.user._id, req.body);
     successResponse(res, 201, "Address added", { addresses });
-  } catch (error) {
-    errorResponse(res, 400, error.message);
-  }
-};
-
-export const verifyPhone = async (req, res) => {
-  try {
-    const { phoneNumber, otp } = req.body;
-    const result = await verifyPhoneNumber(req.user._id, phoneNumber, otp);
-    successResponse(res, 200, "Phone verified", result);
   } catch (error) {
     errorResponse(res, 400, error.message);
   }
