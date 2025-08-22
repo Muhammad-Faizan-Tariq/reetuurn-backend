167b2ad5a9cabc83e3faa3226cbae71b6db81f02
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-08-23 02:53:41 +0500

    Refactor customer route to improve validation handling for phone and address endpoints

--- src/routes/customer.route.js
+++ src/routes/customer.route.js
@@ -2,18 +2,29 @@ import express from "express";
 import {
   getProfile,
   updatePhones,
-  updateCustomerAddresses,
-  createAddress,
-  verifyPhone,
+  addPhone,
+  updateAddresses,
+  addAddress,
 } from "../controllers/customter.controller.js";
+
+import {
+  phoneArrayValidation,
+  singlePhoneValidation,
+  addressArrayValidation,
+  singleAddressValidation,
+  validate,
+} from "../validations/customer.validation.js";
+
 import { verifyToken } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", verifyToken, getProfile);
-router.put("/phones", verifyToken, updatePhones);
-router.put("/addresses", verifyToken, updateCustomerAddresses);
-router.post("/secondary-address", verifyToken, createAddress);
-router.post("/secondary-phone", verifyToken, verifyPhone);
+
+router.put("/phones", verifyToken, phoneArrayValidation, validate, updatePhones);
+router.post("/phones", verifyToken, singlePhoneValidation, validate, addPhone);
+
+router.put("/addresses", verifyToken, addressArrayValidation, validate, updateAddresses);
+router.post("/addresses", verifyToken, singleAddressValidation, validate, addAddress);
 
 export default router;
