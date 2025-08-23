aaa09e74f7acfc81dc6c1b55ae2b366abfd36c86
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-08-24 02:04:39 +0500

    Refactor changePasswordValidator to simplify password validation and remove unused checks

--- src/validations/password.validation.js
+++ src/validations/password.validation.js
@@ -1,24 +1,9 @@
 import { check, body, validationResult } from "express-validator";
-import AuthUser from "../models/authuser.model.js";
-import bcrypt from "bcrypt"; 
 
 export const changePasswordValidator = [
   check("currentPassword")
     .notEmpty()
-    .withMessage("Current password is required")
-    .isLength({ min: 8 })
-    .withMessage("Password must be at least 8 characters")
-    .custom(async (value, { req }) => {
-      const user = await AuthUser.findById(req.user._id).select("+password");
-      if (!user) {
-        throw new Error("User not found");
-      }
-      const isMatch = await user.comparePassword(value);
-      if (!isMatch) {
-        throw new Error("Current password is incorrect");
-      }
-      return true;
-    }),
+    .withMessage("Current password is required"),
 
   body("newPassword")
     .notEmpty()
@@ -37,30 +22,8 @@ export const changePasswordValidator = [
       if (value === req.body.currentPassword) {
         throw new Error("New password must be different from current password");
       }
-
-      const user = await AuthUser.findById(req.user._id).select(
-        "+passwordHistory"
-      );
-      if (user.passwordHistory) {
-        const isUsed = user.passwordHistory.some((record) =>
-          bcrypt.compareSync(value, record.password)
-        );
-        if (isUsed) {
-          throw new Error("Cannot reuse previous passwords");
-        }
-      }
       return true;
-    }),
-
-  body("passwordConfirm")
-    .notEmpty()
-    .withMessage("Please confirm your password")
-    .custom((value, { req }) => {
-      if (value !== req.body.newPassword) {
-        throw new Error("Passwords do not match");
-      }
-      return true;
-    }),
+    })
 ];
 
 export const privacyPreferencesValidator = [
@@ -71,15 +34,12 @@ export const privacyPreferencesValidator = [
 ];
 
 export const validate = (req, res, next) => {
-  const errors = validationResult(req); // <-- ab ye available hai
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).json({
       success: false,
       message: "Validation failed",
-      errors: errors.array().map((err) => ({
-        field: err.param,
-        message: err.msg,
-      })),
+      errors: errors.array(),
     });
   }
   next();
