5b0dcdf32e2921ce844fc6cc2aa671e93ddf6c32
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-08-24 02:03:01 +0500

    Refactor verifyToken middleware to enhance user verification and error handling

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,10 +1,11 @@
 import jwt from "jsonwebtoken";
+import AuthUser from "../models/authuser.model.js";
 
-export const verifyToken = (req, res, next) => {
+export const verifyToken = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
 
   if (!authHeader) {
-    console.log("❌ No Authorization header found");
+    // console.log("No Authorization header found");
     return res
       .status(401)
       .json({ success: false, message: "Unauthorized user" });
@@ -13,7 +14,7 @@ export const verifyToken = (req, res, next) => {
   const token = authHeader.split(" ")[1];
 
   if (!token) {
-    console.log("❌ No token found in Authorization header");
+    // console.log("No token found in Authorization header");
     return res
       .status(401)
       .json({ success: false, message: "Unauthorized user" });
@@ -21,11 +22,27 @@ export const verifyToken = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
-    req.user = decoded;
-    console.log("✅ Token verified. Decoded user:", decoded);
+    
+   
+    const user = await AuthUser.findById(decoded._id);
+    if (!user) {
+      // console.log("User not found in database");
+      return res.status(401).json({ success: false, message: "User not found" });
+    }
+
+    if (user.passwordChangedAt && decoded.iat < Math.floor(user.passwordChangedAt.getTime() / 1000)) {
+      // console.log("Password was changed after token issued");
+      return res.status(401).json({
+        success: false,
+        message: "Password was changed. Please login again."
+      });
+    }
+
+    req.user = user;
+    // console.log("Token verified. User:", user.email);
     next();
   } catch (err) {
-    console.log("❌ Token verification failed:", err.message);
+    // console.log("Token verification failed:", err.message);
     return res
       .status(401)
       .json({ success: false, message: "Unauthorized user" });
