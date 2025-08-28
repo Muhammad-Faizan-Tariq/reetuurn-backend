8750a1da997e613f22de9fc1fb78c8cdcdbda726
Muhammad Faizan Tariq <mfaizantariq99@gmail.com>
2025-08-28 22:57:10 +0500

    add some change for test

--- src/server.js
+++ src/server.js
@@ -9,5 +9,5 @@ const server = http.createServer(app)
 const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => {
-    console.log(`🚀 >>> Server is running on port ${PORT}`);
+    console.log(`🚀 >>>>>> Server is running on port ${PORT}`);
 })
